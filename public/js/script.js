44192be92278a24dad684a1e3bf1621020fbaaea
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-10-23 18:25:13 +0500

    audio play on start and stop

--- public/js/script.js
+++ public/js/script.js
@@ -12,8 +12,12 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+var start_sound = document.getElementById("myAudio");
+var end_audio = document.getElementById("myAudio1");
+
 document.querySelector('button').addEventListener('click', () => {
   recognition.start();
+  start_sound.play();
 });
 
 recognition.addEventListener('speechstart', () => {
@@ -22,6 +26,8 @@ recognition.addEventListener('speechstart', () => {
 
 recognition.addEventListener('result', (e) => {
   console.log('Result has been detected.');
+
+  end_audio.play();
 
   let last = e.results.length - 1;
   let text = e.results[last][0].transcript;
@@ -34,6 +40,7 @@ recognition.addEventListener('result', (e) => {
 
 recognition.addEventListener('speechend', () => {
   recognition.stop();
+  end_audio.play();
 });
 
 recognition.addEventListener('error', (e) => {
@@ -44,12 +51,16 @@ function synthVoice(text) {
   const synth = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
+  utterance.voice = speechSynthesis.getVoices().filter(function (voice) {
+    return voice.name == "Google US English";
+  })[0];
+  // for local change string with this one "Microsoft Zira Desktop - English (United States)"
   synth.speak(utterance);
 }
 
-socket.on('bot reply', function(replyText) {
+socket.on('bot reply', function (replyText) {
   synthVoice(replyText);
 
-  if(replyText == '') replyText = '(No answer...)';
+  if (replyText == '') replyText = '(No answer)';
   outputBot.textContent = replyText;
 });
